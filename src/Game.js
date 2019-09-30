df679d316ab5abe2618cfd08ce9b9655765c37a6
Sinduja Jagadeesan <sindu@Sindujas-MBP.T-mobile.com>
2019-09-29 23:14:03 -0500

    chore: remove unwanted files

---
+++ src/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Game.css';
+
+export const Game = () => {
+  return (
+    <div className="App">
+      My game will  be designed here
+    </div>
+  );
+}
+
+export default Game;
